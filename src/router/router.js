1362ae1ca9d02c4750644564d071e8b4bcb375a9
antaga04 <antaga04@gmail.com>
2023-06-19 22:35:20 +0200

    Update

--- src/router/router.js
+++ src/router/router.js
@@ -2,16 +2,23 @@ import { initNotFound } from '../pages/404/404';
 import { initHome } from '../pages/home/home';
 import { initProjects } from '../pages/projects/projects';
 
-document.querySelectorAll('.nav-link').forEach((link) => {
+const links = document.querySelectorAll('.nav-link');
+console.log(links);
+
+links.forEach((link) => {
   link.addEventListener('click', (ev) => {
     ev.preventDefault();
 
-    if (ev.target.href != window.location.href) {
-      window.history.pushState({}, '', ev.target.href);
+    const targetHref = ev.target.getAttribute('href');
+    const currentHref = window.location.href;
+
+    if (targetHref !== currentHref) {
+      window.history.pushState({}, '', targetHref);
       handleLocation();
     }
   });
 });
+
 
 const handleLocation = () => {
   const pathname = window.location.pathname;
