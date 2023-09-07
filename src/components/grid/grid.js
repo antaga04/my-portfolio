96a6e552fa663406814abc90bb71a9f68f737bcb
antaga04 <antaga04@gmail.com>
2023-09-08 00:57:41 +0100

    change project card

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -6,24 +6,32 @@ export const gridTemplate = (propList) => {
       ${propList
         .map((prop) => {
           return `
-          <li class="preview-box">
-            <div class="box__header">
-              <h2>${prop.name}</h2>
-            </div>
-          
-            <div class="project__img">
-              <img src="${prop.cover}" alt="${prop.name} preview image" loading="lazy">
-            </div>
-
-            <div class="techs">
+          <li style="display: flex; flex-direction: column; width: calc(100%/2.1);">
+            <div class="preview-box card" style="margin: .5em 0 !important; border-bottom-left-radius: 0; border-bottom-right-radius: 0;">
+              <div class="box__header">
+                <h2>${prop.name}</h2>
+                <div class="read-more">
+                  ${prop.github === "" && prop.demo === "" ? "" : `<button class="more-btn"><div class="more">Read more</div></button>`}
+                  <img class="arrow" src="https://res.cloudinary.com/drsfru9lj/image/upload/v1692125714/Portafolio/arrow_xfcrdc.svg" alt="Arrow">
+                </div>
+              </div>
+            
+              <div class="project__img">
+                <img src="${prop.cover}" alt="${prop.name} preview image" loading="lazy">
+              </div>
+              
+              <div class="techs">
               ${prop.techs.map((tech) => `<p>${tech}</p>`).join('')}
-            </div>
+              </div>
+              </div>
 
-            <div class="project-links">
-              ${prop.github === "" && prop.demo === "" ? `<span class="pill-wrapper">${prop.status}</span>` : ""}
-              ${prop.github === "" && prop.demo === "" ? "" : `<button class="ticket more-btn"><div class="ticket__title more">see more</div></button>`}
-              ${prop.github === "" ? "" : `<a class="ticket" href="${prop.github}" target="_blank"><div class="ticket__title">code <i class="fa-solid fa-code"></i></div></a>`}
-              ${prop.demo === "" ? "" : `<a class="ticket" href="${prop.demo}" target="_blank"><div class="ticket__title">demo <i class="fa-solid fa-share"></i></div></a>`}
+              
+            <div class="preview-box" style="border-top-left-radius: 0; border-top-right-radius: 0;">
+              <div class="project-links">
+                ${prop.github === "" && prop.demo === "" ? `<span class="pill-wrapper">${prop.status}</span>` : ""}
+                ${prop.github === "" ? "" : `<a class="ticket" href="${prop.github}" target="_blank"><div class="ticket__title">code <i class="fa-solid fa-code"></i></div></a>`}
+                ${prop.demo === "" ? "" : `<a class="ticket" href="${prop.demo}" target="_blank"><div class="ticket__title">demo <i class="fa-solid fa-share"></i></div></a>`}
+              </div>
             </div>
           </li>
         `;
